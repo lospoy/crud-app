5a8f1908711f0c0cb316205088782911b665bd4d
Carlos Campoy <campoy.miro@gmail.com>
2022-06-01 15:34:19 -0400

    update files, add delete

--- server.js
+++ server.js
@@ -14,18 +14,30 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     const db = client.db('CRUD-APP')
     const quotesCollection = db.collection('quotes')
 
-    app.use(bodyParser.urlencoded({ extended: true}))
+    // .set() must be placed before .get(), .use() etc.
+    // this tells Express we’re using EJS as the template engine.
+    app.set('view engine', 'ejs')
 
+    // USE
+    // ***************************
+    app.use(bodyParser.urlencoded({ extended: true}))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+
+    // GET
+    // ***************************
     app.get('/', (req, res) => {
-      res.sendFile('/CRUDE' +'/index.html')
+      // res.sendFile('/CRUDE' +'/index.html') <-delete this?
       
       db.collection('quotes').find().toArray()
-      .then(results => {
-        console.log(results)
-      })
-      .catch(error => console.error(error))
+        .then(results => {
+          res.render('index.ejs', {quotes: results})
+        })
+        .catch(error => console.error(error))
     })
 
+    // POST
+    // ***************************
     app.post('/quotes', (req, res) => {
       quotesCollection.insertOne(req.body)
         .then(result => {
@@ -34,6 +46,42 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         .catch(error => console.error(error))
     })
 
+    // PUT
+    // ***************************
+    app.put('/quotes', (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => res.json('Success'))
+        .catch(error => console.error(error))
+    })
+
+    // DELETE
+    // ***************************
+    app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne(
+        { name: req.body.name }
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No quote to delete')
+          }
+          res.json(`Deleted Darth Vadar's quote`)
+        })
+        .catch(error => console.error(error))
+    })
+
+    // LISTEN
+    // ***************************
     app.listen(3000, function() {
       console.log('listening on 3000')
     })
